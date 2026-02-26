7d82f8611a47297b6c96fa6ab41d24c3416dab27
tht1988 <443719519@qq.com>
2026-02-26 19:36:05 +0800

    Update slot-config.js

--- slots/slot-config.js
+++ slots/slot-config.js
@@ -8,7 +8,6 @@ window.slotRarities = [
     { id: 'epic', name: '传说', color: '#ff8000' }
 ];
 
-
 // 各工具可用的插片效果
 window.slotEffects = {
     'pickaxe': [
@@ -59,7 +58,8 @@ window.slotCraftingRecipes = {
         materials: {
             '磁铁': 5,
             '工具插片碎片': 10
-        }
+        },
+        description: '基础工具插片，用于徽章升级和制作各种工具插片'
     },
     'headlightSlot': {
         name: '头灯插片',
@@ -67,7 +67,8 @@ window.slotCraftingRecipes = {
             'toolSlot1': 1,
             '电池': 1,
             'headlightTicket': 1
-        }
+        },
+        description: '头灯专用插片，用于提升头灯性能'
     },
     'cartSlot': {
         name: '矿车插片',
@@ -75,7 +76,8 @@ window.slotCraftingRecipes = {
             'toolSlot1': 1,
             '燃料': 1,
             'cartTicket': 1
-        }
+        },
+        description: '矿车专用插片，用于提升矿车性能'
     },
     'pickaxeSlot': {
         name: '采矿锄插片',
@@ -83,7 +85,8 @@ window.slotCraftingRecipes = {
             'toolSlot1': 1,
             '磁铁': 1,
             'pickaxeTicket': 1
-        }
+        },
+        description: '采矿锄专用插片，用于提升采矿锄性能'
     }
 };
 
